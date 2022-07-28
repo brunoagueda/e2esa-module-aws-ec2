cda4934042cfd11383c135d5d3b708b1ed2539e3
Bruno Agueda <brunoagueda@gmail.com>
2022-07-28 13:37:52 -0300

    Added EBS back again

--- main.tf
+++ main.tf
@@ -30,16 +30,16 @@ resource "aws_instance" "this" {
   tags = merge(var.tags)
 }
 
-#resource "aws_ebs_volume" "this" {
-#  availability_zone = var.availability_zone
-#  size              = 40
-#}
+resource "aws_ebs_volume" "this" {
+  availability_zone = var.availability_zone
+  size              = 40
+}
 
-#resource "aws_volume_attachment" "this" {
-#  device_name = "/dev/sdh"
-#  volume_id   = aws_ebs_volume.this.id
-#  instance_id = aws_instance.this.id
-#}
+resource "aws_volume_attachment" "this" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.this.id
+  instance_id = aws_instance.this.id
+}
 
 #resource "aws_cloudwatch_metric_alarm" "this" {
 #
